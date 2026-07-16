c7fc33865142f9b71b6a8ee43b25f63f03c7f775
1amageek <tmy0x3@icloud.com>
2026-07-17 06:59:02 +0900

    Update standalone dependency revisions

--- Package.swift
+++ Package.swift
@@ -16,25 +16,25 @@ let logicDesignDependency: Package.Dependency = FileManager.default.fileExists(
     atPath: workspaceRoot.appendingPathComponent("LogicDesign/Package.swift").path
 )
     ? .package(path: "../LogicDesign")
-    : .package(url: "https://github.com/1amageek/LogicDesign.git", revision: "8e0c8c2c63152aa45bf12d943fa034bb1aba0f1e")
+    : .package(url: "https://github.com/1amageek/LogicDesign.git", revision: "cc39c974bf14624e6ce29fd8722620385fde0762")
 
 let timingEngineDependency: Package.Dependency = FileManager.default.fileExists(
     atPath: workspaceRoot.appendingPathComponent("TimingEngine/Package.swift").path
 )
     ? .package(path: "../TimingEngine")
-    : .package(url: "https://github.com/1amageek/TimingEngine.git", revision: "5b2f711d355af8a204819c6ed33f98ef722e379c")
+    : .package(url: "https://github.com/1amageek/TimingEngine.git", revision: "fa906d67d86f90fac8850cd3036cc5546a0904c4")
 
 let toolQualificationDependency: Package.Dependency = FileManager.default.fileExists(
     atPath: workspaceRoot.appendingPathComponent("ToolQualification/Package.swift").path
 )
     ? .package(path: "../ToolQualification")
-    : .package(url: "https://github.com/1amageek/ToolQualification.git", revision: "32b031b5322f1ccb0ef78466faab0f895d47c4fd")
+    : .package(url: "https://github.com/1amageek/ToolQualification.git", revision: "1856a1bc5660febbe2f0358d3e5e0262e496b3d3e")
 
 let logicEngineDependency: Package.Dependency = FileManager.default.fileExists(
     atPath: workspaceRoot.appendingPathComponent("LogicEngine/Package.swift").path
 )
     ? .package(path: "../LogicEngine")
-    : .package(url: "https://github.com/1amageek/LogicEngine.git", revision: "5cccdb82e7272b407642aae0708966d62ceffba4")
+    : .package(url: "https://github.com/1amageek/LogicEngine.git", revision: "5604a971a8a8b4246c9a2ac79efe780f40ad4b65")
 
 let package = Package(
     name: "RTLVerificationEngine",
